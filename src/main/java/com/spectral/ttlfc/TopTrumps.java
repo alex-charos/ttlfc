48b1dda2f4518b814c0ec53372e9feaa89b51793
Alex <realtowz@yahoo.com>
2016-02-10 20:40:55 +0200

    added scheduling. Host Polls heartbeat map to boot players that haven't pinged

--- src/main/java/com/spectral/ttlfc/TopTrumps.java
+++ src/main/java/com/spectral/ttlfc/TopTrumps.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
+@EnableScheduling
 public class TopTrumps {
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(TopTrumps.class, args);
--- src/main/java/com/spectral/ttlfc/service/Host.java
+++ src/main/java/com/spectral/ttlfc/service/Host.java
@@ -1,5 +1,6 @@
 package com.spectral.ttlfc.service;
 
+import java.util.Date;
 import java.util.UUID;
 
 import com.spectral.ttlfc.model.Player;
@@ -9,5 +10,6 @@ public interface Host {
 	
 	PlayerEntryResponse acceptPlayer(Player p);
 	PlayerEntryResponse getPlayerStatus(UUID playerUUID);
-
+	void checkPlayersPresence();
+	Date playerHeartbeat(UUID playerId);
 }
